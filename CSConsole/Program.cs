092833bd8ce34c7d4be690736bf2d34e7d25fc09
Luke Byrne <luke.byrne@azyra.com>
2022-11-30 08:59:26 +0000

    Added C# Console to test with

---
+++ CSConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test;
+
+namespace CSConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var xsArray = XLib.Functions.Test_XS();
+            var csArray = CSClass.Test_CS();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
